d40dd8a38b134a1783b457e9f07e25dc5a57a30f
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2020-11-18 17:40:12 -0500

    add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      post '/login',    to: 'sessions#create'
+      post '/logout',   to: 'sessions#destroy'
+      get '/logged_in', to: 'sessions#is_logged_in?'
       resources :users do
         resources :categorys do
           resources :tasks do
@@ -12,4 +15,5 @@ Rails.application.routes.draw do
       end
     end
   end
+ 
 end
