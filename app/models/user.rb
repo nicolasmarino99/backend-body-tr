e737e7d87a9c2dfcf4ba7d668a487773606a6e08
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2020-11-18 17:00:56 -0500

    update model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   has_many :progess_items
   has_many :categorys
 
+  has_secure_password
+  
   # validation
-  validates_presence_of :name, :password, :email, :weight, :height
+  validates :name, uniqueness: true
+  validates :name, length: { minimum: 4 }
+  validates_presence_of :name, :password_digest, :email, :weight, :height
 end
