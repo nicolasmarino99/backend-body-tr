3bc2406d835627287d71640487190447d4b924d6
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2020-11-18 12:08:22 -0500

    add associations to models

--- app/models/progress_item.rb
+++ app/models/progress_item.rb
@@ -1,3 +1,4 @@
 class ProgressItem < ApplicationRecord
   belongs_to :task
+  validates_presence_of :metric, :amount
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :category
+  has_many :progess_item
+
+  validates_presence_of :name
 end
