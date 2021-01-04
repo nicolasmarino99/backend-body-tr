ef56a7c61bb8cd5ec542a71484b87e79dbea4ca0
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2021-01-04 14:17:08 -0500

    add right url to production session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
-if Rails.env === 'production' 
-    Rails.application.config.session_store :cookie_store, key: '_backend-body-tr', domain: 'backend-body-tr.herokuapp.com'
-  else
-    Rails.application.config.session_store :cookie_store, key: '_backend-body-tr'
-  end
+if Rails.env === 'production'
+  Rails.application.config.session_store :cookie_store, key: '_backend-body-tr', domain: 'https://backend-body-tr.herokuapp.com/api/v1/logged_in'
+else
+  Rails.application.config.session_store :cookie_store, key: '_backend-body-tr'
+end
